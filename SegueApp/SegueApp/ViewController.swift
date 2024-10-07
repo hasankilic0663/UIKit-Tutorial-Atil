84bbbff625e514f5e67eaa1592291e1a81416030
Hasan Hüseyin KILIÇ <hasankilic0663@hotmail.com>
2024-10-07 11:48:37 +0300

    SegueApp ve Lifecycle öğrenildi. Ekranlar arası IDye göre aktarım belirlendi

--- SegueApp/SegueApp/ViewController.swift
+++ SegueApp/SegueApp/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userName = ""
     
     
     @IBOutlet weak var nameText: UITextField!
@@ -16,16 +17,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Lifecycle
+        print("viewdidload")
+    }
+//yaşam döngüleri
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewdiddisappear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewwilldisappear")
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewwillappear")
+        nameText.text = ""
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewdidappear")
     }
 
     
+
+    
     @IBAction func nextButton2(_ sender: Any) {
+        
+        userName = nameText.text!
+        //next butonuna tıkladıgında segueyı gerceklestır ve 2. ekrana gec
         performSegue(withIdentifier: "toSecondVC", sender: nil) // bilgi yollama sender . burda ekranı aktarma
         
     }
-    //segue olmadan hemen yapılacak ıslemlerı buraya yazabıluyorsun 
+    //segue olmadan hemen yapılacak ıslemlerı buraya yazabıluyorsun. override edioypz yani üstüne yazıyoruz .
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        <#code#>
+        if segue.identifier == "toSecondVC"{ // bunu yazmamızın amacı dogru ekranın idsi ile eslestırıp o ekranla ılgılı ıslemlerın yapıldıgı yer gıbi düşünebilirz.
+            //as kullanmamlazım
+            let destinationVC = segue.destination  as! SecondViewController //gidilen hedef yer neyse onu verıyor
+            destinationVC.myName = userName // burada diğer ekrana  burdakı yazıyı veriyi aktardık 
+            
+            
+            
+        }
     }
 }
 
