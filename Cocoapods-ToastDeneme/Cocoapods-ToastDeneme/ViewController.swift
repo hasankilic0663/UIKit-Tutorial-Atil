cb0aad9af23081b37909056c61c5e911e34d36a0
Hasan Hüseyin KILIÇ <hasankilic0663@hotmail.com>
2024-10-14 17:48:10 +0300

    commited cocoapods-toast

---
+++ Cocoapods-ToastDeneme/Cocoapods-ToastDeneme/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Cocoapods-ToastDeneme
+//
+//  Created by Hasan Hüseyin Kılıç on 14.10.2024.
+//
+
+import UIKit
+import Toast
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.makeToast("selam")
+    }
+
+
+}
+
