6c9b0e0076e8e28fb01c169366715d2bb4cd963c
Hasan Hüseyin KILIÇ <hasankilic0663@hotmail.com>
2024-10-15 10:17:45 +0300

    Cocoapods denemesi başarısız.

--- Cocoapods-ToastDeneme/Cocoapods-ToastDeneme/ViewController.swift
+++ Cocoapods-ToastDeneme/Cocoapods-ToastDeneme/ViewController.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
-import Toast
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.makeToast("selam")
+       
     }
 
 
