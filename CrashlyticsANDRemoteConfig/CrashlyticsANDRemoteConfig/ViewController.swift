6e18dfa24c357a6d43de1bdaad340a9a54b5ea42
Hasan Hüseyin KILIÇ <hasankilic0663@hotmail.com>
2024-10-21 14:58:11 +0300

    CrashlyticsANDRemoteConfig created .

---
+++ CrashlyticsANDRemoteConfig/CrashlyticsANDRemoteConfig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrashlyticsANDRemoteConfig
+//
+//  Created by Hasan Hüseyin Kılıç on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
